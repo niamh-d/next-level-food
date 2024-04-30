ef9c304759925b840c3674cf9659e093cf0219bb
Niamh <niamh.linguist@gmail.com>
2024-04-30 21:04:39 +0300

    refactoring header code

---
+++ components/main-header/main-header.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import MainHeaderBackground from "./main-header-background";
+
+import logoImg from "@/assets/logo.png";
+
+import styles from "./main-header.module.css";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link href="/" className={styles.logo}>
+          <Image src={logoImg} alt="Next Level Food logo" priority />
+          Next Level Food
+        </Link>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
