a4e761ec7f1cb1cd69a07ea18cf3363b24a96805
Niamh <niamh.linguist@gmail.com>
2024-05-01 16:42:20 +0300

    added content to meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 export default function MealsPage() {
   return (
     <div>
-      <h1>Meals</h1>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favouriterecipe and cook it yourself. It&apos;s easy and
+          fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share your favourite recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}></main>
     </div>
   );
 }
